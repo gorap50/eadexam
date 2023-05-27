7750287f25d82a7f07af2f0ed937a60c2d44a683
gorap50 <76959427+gorap50@users.noreply.github.com>
2023-05-27 17:19:19 +0500

    Add Thankyou Message

---
+++ src/components/ThankyouMessage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ThankyouMessage() {
+    
+  
+   
+  return (
+    <div>
+      <button onClick={()=>{alert ("Thankyou")}}>Submit</button>
+     
+    </div>
+  )
+}
+
+export default ThankyouMessage
