38536e888aacc11dcaed6c7363a8c2107e72c52c
gorap50 <76959427+gorap50@users.noreply.github.com>
2023-05-27 17:03:36 +0500

    Add User Particpation

--- src/components/UserParticipation.jsx
+++ src/components/UserParticipation.jsx
@@ -6,25 +6,69 @@ function UserParticipation() {
     const [PyCount,setPyCount]=useState(0)
     const [JavaCount,setJavaCount]=useState(0)
     const [CsCount,setCsCount]=useState(0)
+
+
   return (
     <div>
       
       <h3>Answer the Choices Select</h3>
-      <button onClick={()=>setJsCount(JsCount+1)}>JavaScript</button> 
+
+      
+      
+      <input type="checkbox" id="one" name="checkbox" onSelect={()=>{const checkbox = document.getElementById('one')
+
+checkbox.addEventListener('change', (event) => {
+  if (event.currentTarget.checked) {
+    JsCount=setJsCount(prevCount=>prevCount+1)
+  } else {
+    alert('not checked');
+  }
+})}} />
+      <label for="one">JavaScript</label>
       <h2>Total JavaScript Votes: {JsCount}</h2><br />
 
      
+      <input type="checkbox" id="two" name="checkbox" onSelect={()=>{const checkbox = document.getElementById('two')
 
-      <button onClick={()=>setPyCount(PyCount+1)}>Python</button>
+checkbox.addEventListener('change', (event) => {
+  if (event.currentTarget.checked) {
+    PyCount=setPyCount(prevCount=>prevCount+1)
+  } else {
+    alert('not checked');
+  }
+})}} />
+      <label for="two">Python</label>
       <h2>Total Python Votes: {PyCount}</h2> <br />
 
-      <button onClick={()=>setJavaCount(JavaCount+1)}>Java</button>
+
+
+      <input type="checkbox" id="three" name="checkbox" onSelect={()=>{const checkbox = document.getElementById('three')
+
+checkbox.addEventListener('change', (event) => {
+  if (event.currentTarget.checked) {
+    JavaCount=setJavaCount(prevCount=>prevCount+1)
+  } else {
+    alert('not checked');
+  }
+})}} />
+      <label for="three">Java</label>
       <h2>Total Java Votes: {JavaCount}</h2> <br />
 
-      <button onClick={()=>setCsCount(CsCount+1)}>CSharp</button> 
-      <h2>Total CSharp Votes: {CsCount}</h2>
+      
+      <input type="checkbox" id="four" name="checkbox" onSelect={()=>{const checkbox = document.getElementById('three')
 
-      <input type="checkbox" name="JavaScript" title='JavaScript' id="" />
+checkbox.addEventListener('change', (event) => {
+  if (event.currentTarget.checked) {
+    CsCount=setCsCount(prevCount=>prevCount+1)
+  } else {
+    alert('not checked');
+  }
+})}} />
+      <label for="four">CSharp</label>
+      <h2>Total CSharp Votes: {CsCount}</h2>
+      
+
+      
 
     </div>
   )
